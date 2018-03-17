5cc3abfbf069a497e5779c3d6f67bc7f6a96f8b4
today4u <today4u@users.noreply.github.com>
2018-03-17 22:34:28 +0900

    add: Express

---
+++ socket/routes/module/mod_socket.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type':'text/html'});
+  res.end('server connected');
+});
+var io = require('socket.io').listen(server);
+
+server.listen(8888);
+
+io.sockets.on('connection', function (socket) {
+  console.log('connected!');
+});
+
