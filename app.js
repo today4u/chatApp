d4eaca4d209998805db602028746dbfa4ffb1198
today4u <today4u@users.noreply.github.com>
2018-03-19 04:07:58 +0900

    change: sample source

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var socket = require('./routes/module/mod_socket.js');
 
 var app = express();
 
